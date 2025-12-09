99ce545a8e8077dfe9513b2152f0458991f94876
Arome8240 <arome8240@gmail.com>
2025-12-09 10:23:27 +0100

    d

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    // Ignore test files in problematic node_modules packages
+    config.externals.push({
+      // prevent test files from being bundled
+      'tap': 'commonjs tap',
+    });
+
+    config.module.rules.push({
+      test: /thread-stream\/test|pino\/test/,
+      use: 'null-loader',
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
